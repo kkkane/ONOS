9c3689db57514e30349873e1745f847e32f8f0a9
kkkane <k951874632@gmail.com>
2015-10-14 20:16:37 +0800

    modify hello to simple topology send

--- apps/hello/src/main/java/org/onosproject/hello/Hello.java
+++ apps/hello/src/main/java/org/onosproject/hello/Hello.java
@@ -5,7 +5,6 @@ import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Deactivate;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.ReferenceCardinality;
-
 import org.onosproject.core.ApplicationId;
 import org.onosproject.core.CoreService;
 import org.onosproject.net.Host;
@@ -16,7 +15,7 @@ import org.onosproject.net.flow.DefaultTrafficTreatment;
 import org.onosproject.net.flow.TrafficSelector;
 import org.onosproject.net.flow.TrafficTreatment;
 import org.onosproject.net.host.HostService;
-import org.onosproject.net.intent.HostToHostIntent;
+//import org.onosproject.net.intent.HostToHostIntent;
 import org.onosproject.net.intent.IntentService;
 import org.onosproject.net.packet.DefaultOutboundPacket;
 import org.onosproject.net.packet.InboundPacket;
@@ -27,46 +26,38 @@ import org.onosproject.net.packet.PacketProcessor;
 import org.onosproject.net.packet.PacketService;
 import org.onosproject.net.topology.TopologyService;
 import org.onlab.packet.Ethernet;
+//import org.onlab.packet.IpAddress;
+import org.onlab.packet.Ip4Address;
+import org.onlab.packet.Ip4Prefix;
 import org.slf4j.Logger;
-
 import static org.slf4j.LoggerFactory.getLogger;
-
 /**
- * Sample reactive forwarding application.
- */
+* Sample reactive forwarding application.
+*/
 @Component(immediate = true)
 public class Hello {
-
     // for verbose output
     private final Logger log = getLogger(getClass());
-
     // a list of our dependencies :
     // to register with ONOS as an application - described next
     @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
     protected CoreService coreService;
-
     // topology information
     @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
     protected TopologyService topologyService;
-
     // to receive Packet-in events that we'll respond to
     @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
     protected PacketService packetService;
-
     // to submit/withdraw intents for traffic manipulation
     @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
     protected IntentService intentService;
-
     // end host information
     @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
     protected HostService hostService;
-
     // our application-specific event handler
     private ReactivePacketProcessor processor = new ReactivePacketProcessor();
-
     // our unique identifier
     private ApplicationId appId;
-
     // method called at startup
     @Activate
     public void activate() {
@@ -74,19 +65,19 @@ public class Hello {
         appId = coreService.registerApplication("org.onosproject.hello");
         // register our event handler
         packetService.addProcessor(processor, PacketProcessor.ADVISOR_MAX + 2);
-
         TrafficSelector.Builder selector = DefaultTrafficSelector.builder();
-        //TrafficSelector.Builder selector2 = DefaultTrafficSelector.builder();
 
-        //datapath_join event
-        selector.matchEthType(Ethernet.TYPE_IPV4);
-
+        //set match:ARP, IPV4 action:controller
+        /*selector.matchEthType(Ethernet.TYPE_ARP);
         packetService.requestPackets(selector.build(), PacketPriority.REACTIVE, appId);
+        selector.matchEthType(Ethernet.TYPE_IPV4);
+        packetService.requestPackets(selector.build(), PacketPriority.REACTIVE, appId);*/
+
+        defaultRuleForSW();
 
         log.info("SSSSSSSSSSSSSSSSSStarteddddddddddddd");
         //System.out.println("SSSSSSSSSSSSSSSSSstarteddddddddddddd");
     }
-
     // method called at shutdown
     @Deactivate
     public void deactivate() {
@@ -96,11 +87,10 @@ public class Hello {
         log.info("SSSSSSSSStoppedddddddddddddd");
         //System.out.println("SSSSSSSSSSStoppeddddddddddddd");
     }
-
     // our handler defined as a private inner class
     /**
-     * Packet processor responsible for forwarding packets along their paths.
-     */
+    * Packet processor responsible for forwarding packets along their paths.
+    */
     private class ReactivePacketProcessor implements PacketProcessor {
         @Override
         public void process(PacketContext context) {
@@ -110,61 +100,57 @@ public class Hello {
             if (context.isHandled()) {
                 return;
             }
-
             //System.out.println("Hello b!");
             // Extract the original Ethernet frame from the packet information
             InboundPacket pkt = context.inPacket();
             Ethernet ethPkt = pkt.parsed();
-
             // Find and source and destination hosts
             HostId srcId = HostId.hostId(ethPkt.getSourceMAC());
             HostId dstId = HostId.hostId(ethPkt.getDestinationMAC());
-
-            // Do we know who this is for? If not, flood and bail.
-            Host dst = hostService.getHost(dstId);
-            if (dst == null) {
-                flood(context);
-                return;
-            }
-
-            // Otherwise forward and be done with it.
-            setUpConnectivity(context, srcId, dstId);
-            forwardPacketToDst(context, dst);
+            packetInProcess(pkt);
         }
     }
-
-    // Floods the specified packet if permissible.
-    private void flood(PacketContext context) {
-        if (topologyService.isBroadcastPoint(topologyService.currentTopology(),
-                                         context.inPacket().receivedFrom())) {
-            packetOut(context, PortNumber.FLOOD);
-        } else {
-            context.block();
-        }
-    }
-
     // Sends a packet out the specified port.
     private void packetOut(PacketContext context, PortNumber portNumber) {
         context.treatmentBuilder().setOutput(portNumber);
         context.send();
+        //log.info("A. sending setOutput");
     }
-
     private void forwardPacketToDst(PacketContext context, Host dst) {
         TrafficTreatment treatment = DefaultTrafficTreatment.builder().setOutput(dst.location().port()).build();
         OutboundPacket packet = new DefaultOutboundPacket(dst.location().deviceId(),
-                                                      treatment, context.inPacket().unparsed());
+        treatment, context.inPacket().unparsed());
+        log.info("D. DevicId {}", dst.location().deviceId());
         packetService.emit(packet);
-        log.info("ssssssssssssssendinggggg packet: {}", packet);
+        log.info("B. sending packet: {}", packet);
         //System.out.print("sendinggggg packet: ");
         //System.out.println(packet);
     }
-
-    // Install a rule forwarding the packet to the specified port.
-    private void setUpConnectivity(PacketContext context, HostId srcId, HostId dstId) {
-        TrafficSelector selector = DefaultTrafficSelector.builder().build();
-        TrafficTreatment treatment = DefaultTrafficTreatment.builder().build();
-        HostToHostIntent intent = HostToHostIntent.builder()
-        .appId(appId).one(srcId).two(dstId).selector(selector).treatment(treatment).build();
-        intentService.submit(intent);
+    private void defaultRuleForSW() {
+        TrafficSelector.Builder selectorBuilder = DefaultTrafficSelector.builder();
+        byte[] host1 = {0x10, 0x0, 0x0, 0x1};
+        byte[] host2 = {0x10, 0x0, 0x0, 0x2};
+        Ip4Address ip1 = Ip4Address.valueOf(host1);
+        Ip4Address ip2 = Ip4Address.valueOf(host2);
+        Ip4Prefix matchIp4SrcPrefix1 = Ip4Prefix.valueOf(ip1, Ip4Prefix.MAX_MASK_LENGTH);
+        Ip4Prefix matchIp4SrcPrefix2 = Ip4Prefix.valueOf(ip2, Ip4Prefix.MAX_MASK_LENGTH);
+        selectorBuilder.matchIPSrc(matchIp4SrcPrefix1);
+        packetService.requestPackets(selectorBuilder.build(), PacketPriority.REACTIVE, appId);
+        selectorBuilder.matchIPSrc(matchIp4SrcPrefix2);
+        packetService.requestPackets(selectorBuilder.build(), PacketPriority.REACTIVE, appId);
+    }
+    private void packetInProcess(InboundPacket pkt) {
+        byte[] host1 = {0x10, 0x0, 0x0, 0x1};
+        byte[] host2 = {0x10, 0x0, 0x0, 0x2};
+        Ip4Address ip1 = Ip4Address.valueOf(host1);
+        Ip4Address ip2 = Ip4Address.valueOf(host2);
+        log.info("Pkt. IP src {}", pkt.receivedFrom().ipElementId().ipAddress().getIp4Address());
+        Ip4Address srcIp = pkt.receivedFrom().ipElementId().ipAddress().getIp4Address();
+        if (srcIp.equals(ip1)) {
+            log.info("Pkt. src {} you are h1", pkt.receivedFrom().ipElementId().ipAddress());
+        }
+        if (srcIp.equals(ip2)) {
+            log.info("Pkt. src {} you are h2", pkt.receivedFrom().ipElementId().ipAddress());
+        }
     }
 }
